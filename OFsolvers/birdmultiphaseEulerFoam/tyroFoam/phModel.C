c6bc01ebfc62d8b2e77f84c3d67f1518fc2b7888
JuliaAReam <juliaream17@gmail.com>
2026-04-10 16:46:29 -0600

    Add MEML switches to pH components for tyro

--- OFsolvers/birdmultiphaseEulerFoam/tyroFoam/phModel.C
+++ OFsolvers/birdmultiphaseEulerFoam/tyroFoam/phModel.C
@@ -1,9 +1,12 @@
 #include "phModel.H"
-#include<map>
-//#include<vector>
+#include <map>
+#include <vector>
+#include <string>
+#include <cmath>
 
 namespace acidbasemodel
 {
+  // PB=HPO4, PA=H2PO4, NB=NH3, NA=NH4, AA=CH3COOH, BA=C3H7COOH
   const int PB = 0;
   const int PA = 1;
   const int NB = 2;
@@ -66,29 +69,33 @@ namespace acidbasemodel
     totalConc[nvars/2+1] = -Z;
   }
 
-  double charge(double H, std::vector<double>& totalConc)
+  double charge(double H, std::vector<double>& totalConc, bool do_ml)
   {
     // getRatio; // figure this out later...
 
-    double ratio = 2.0;
+    double mm_ratio = do_ml ? 0.0 : 1.0;
+    double meml_ratio = do_ml ? 1.0 : 0.0;
     double ch = H						\
       + totalConc[AmmoniaBase] * H / (H + Ka[AmmoniaBase])	\
-      - totalConc[AceticAcid] * H / (H + Ka[AceticAcid]) \
-      - totalConc[ButyricAcid] * H / (H + Ka[ButyricAcid]) \
       - totalConc[PhosphateBuffer] * H / (H + Ka[PhosphateBuffer]) \
       - 2.0 * totalConc[PhosphateBuffer] * Ka[PhosphateBuffer] / (H + Ka[PhosphateBuffer]) \
       - Kw / H \
-      + totalConc[nvars/2+1];
-
+      + totalConc[nvars/2+1]
+      - mm_ratio * totalConc[AceticAcid] * H / (H + Ka[AceticAcid])	   \
+      - mm_ratio * totalConc[ButyricAcid] * H / (H + Ka[ButyricAcid]) \
+      - meml_ratio * totalConc[nvars/2 + 2];
     return ch;
   }
 
-  double dchargedH(double H, std::vector<double>& totalConc)
+  double dchargedH(double H, std::vector<double>& totalConc, bool do_ml)
   {
+
+    double mm_ratio = do_ml ? 0.0 : 1.0;
+    
     double dchdH = 1.0 \
       + Ka[AmmoniaBase] * totalConc[AmmoniaBase] / ((Ka[AmmoniaBase] + H) * (Ka[AmmoniaBase] + H)) \
-      - Ka[ButyricAcid] * totalConc[ButyricAcid] / ((Ka[ButyricAcid] + H) * (Ka[ButyricAcid] + H)) \
-      - Ka[AceticAcid] * totalConc[AceticAcid] / ((Ka[AceticAcid] + H) * (Ka[AceticAcid] + H)) \
+      - mm_ratio * Ka[ButyricAcid] * totalConc[ButyricAcid] / ((Ka[ButyricAcid] + H) * (Ka[ButyricAcid] + H)) \
+      - mm_ratio * Ka[AceticAcid] * totalConc[AceticAcid] / ((Ka[AceticAcid] + H) * (Ka[AceticAcid] + H)) \
       - Ka[PhosphateBuffer] * totalConc[PhosphateBuffer] / ((Ka[PhosphateBuffer] + H) * (Ka[PhosphateBuffer] + H)) \
       + 2.0 * totalConc[PhosphateBuffer] * Ka[PhosphateBuffer] / ((Ka[PhosphateBuffer] + H) * (Ka[PhosphateBuffer] + H)) \
       + Kw/(H*H);
@@ -96,22 +103,28 @@ namespace acidbasemodel
     return dchdH;
   }
 
-  double NewtonRaphson(double H_init, std::vector<double>& totalConc)
+  double NewtonRaphson(double H_init, std::vector<double>& totalConc, bool do_ml)
   {
     double tol = 1.0E-14;
-    double test = charge(H_init, totalConc);
+    const double H_floor = 1.0E-14;
+    double H_seed = H_init > H_floor ? H_init : H_floor;
+    double test = charge(H_init, totalConc, do_ml);
     // std::cout << "Initialized Charge is: " << test << "\n";
     double H_next = 0.0;
-    double H_now = H_init;
+    double H_now = H_seed;
     int iter = 0;
     while(std::abs(test) > tol && iter<100)
       {
-	double chargeCurrent = charge(H_now, totalConc);
-	double dchargedHCurrent = dchargedH(H_now, totalConc);
+	double chargeCurrent = charge(H_now, totalConc, do_ml);
+	double dchargedHCurrent = dchargedH(H_now, totalConc, do_ml);
 	// std::cout << "current Charge: " << chargeCurrent << "\n";
 	// std::cout << "current derivative: " << dchargedHCurrent << "\n";
-	H_next =  H_now - charge(H_now, totalConc)/dchargedH(H_now, totalConc);
-	test = charge(H_next, totalConc);
+	H_next =  H_now - charge(H_now, totalConc, do_ml)/dchargedH(H_now, totalConc, do_ml);
+	if (!(H_next > H_floor))
+	  {
+	    H_next = H_floor;
+	  }
+	test = charge(H_next, totalConc, do_ml);
 	H_now = H_next;
 	// std::cout << "\n";
 	// std::cout << "Updated Charge: " << test << "\n";
